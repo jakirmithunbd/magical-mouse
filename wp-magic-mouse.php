9f8236f93c819dab65e9a1dcf342eb2bebefa6f1
Tanin Ahmed <taninakond@gmail.com>
2023-11-22 17:23:08 +0600

    update author name

--- wp-magic-mouse.php
+++ wp-magic-mouse.php
@@ -1,26 +1,26 @@
 <?php
 /**
  * WpMagicMouse
-*/ 
+ */
 
 /*
  * Plugin Name:       WP Magic Mouse
- * Plugin URI:        
+ * Plugin URI:
  * Description:       This is a plugin for adding custom cursor with animation in WordPress website
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Jakir
+ * Author:            CodeConfig
  * Author URI:        https://wpwebdevs.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wpmm
  * Domain Path:       /languages
-*/
+ */
 
 defined( 'ABSPATH' ) or die( 'Hey, what are you doing here? You silly human!' );
 
-if ( file_exists(dirname( __FILE__ ) . '/vendor/autoload.php') ) {
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
     require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
@@ -30,14 +30,14 @@ if ( file_exists(dirname( __FILE__ ) . '/vendor/autoload.php') ) {
 
 register_activation_hook( __FILE__, 'activate_wpmm_plugin' );
 function activate_wpmm_plugin() {
-	Inc\Base\Activate::activate();
+    Inc\Base\Activate::activate();
 }
 
 /**
  * The code that runs during plugin deactivation
  */
 function deactivate_wpmm_plugin() {
-	Inc\Base\Deactivate::deactivate();
+    Inc\Base\Deactivate::deactivate();
 }
 register_deactivation_hook( __FILE__, 'deactivate_wpmm_plugin' );
 
@@ -45,5 +45,5 @@ register_deactivation_hook( __FILE__, 'deactivate_wpmm_plugin' );
  * Initialize all the core classes of the plugin
  */
 if ( class_exists( 'Inc\\Init' ) ) {
-	Inc\Init::register_services();
+    Inc\Init::register_services();
 }
